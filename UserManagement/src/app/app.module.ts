2fc82c1f98d9c5b90747c158df94f18f8336cc03
niranjannaidu95 <77660937+niranjannaidu95@users.noreply.github.com>
2021-03-18 14:30:39 +0530

    my commit

--- UserManagement/src/app/app.module.ts
+++ UserManagement/src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
+import { TokenInterceptorService } from './token-interceptor.service';
+import { AuthuserComponent } from './authuser/authuser.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    ErrorComponent,
+    HomeComponent,
+    AuthuserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [AuthService, 
+  {
+    provide:HTTP_INTERCEPTORS,
+    useClass:TokenInterceptorService,
+    multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
